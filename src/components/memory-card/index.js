7ea260b6bfe291d0b505c428d0b09d24a67b1cf8
Raphael Divetta Bronzo <divetta@gmail.com>
2019-01-22 00:12:55 +0000

    Fim da aula 024

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -1,34 +1,33 @@
 function createMemoryCard() {
-  const $memoryCard = document.createElement("article");
-
-  const $iconCollab = `
-    <img  
-      src='img/icon-collabcode.png'
-      alt='Gueio mascote da CollabCode' 
-      class='icon'
-    />
+  const $memoryCard = `
+    <article class="memory-card">
+      <img  
+        src='img/icon-collabcode.png'
+        alt='Gueio mascote da CollabCode' 
+        class='icon'
+        onClick="handleClick()"
+      />    
+    </article>
   `;
 
-  $memoryCard.classList.add("memory-card");
-  $wrapCards.insertBefore($memoryCard, null);
-
-  $memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+  return $memoryCard;
 }
 
 function createMemoryCardFront() {
-  const $memoryCardFront = document.createElement("article");
-
-  const $iconC = `
-    <img  
-      src='img/icon-c.png'
-      alt='Gueio mascote da CollabCode' 
-      class='icon'
-    />
+  const $memoryCard = `
+    <article class="memory-card -front">
+      <img  
+        src='img/icon-c.png'
+        alt='Gueio mascote da CollabCode' 
+        class='icon'
+        onClick="handleClick()"
+      />    
+    </article>
   `;
 
-  $memoryCardFront.classList.add("memory-card");
-  $memoryCardFront.classList.add("-front");
-  $wrapCards.insertBefore($memoryCardFront, null);
+  return $memoryCard;
+}
 
-  $memoryCardFront.insertAdjacentHTML("afterbegin", $iconC);
+function handleClick() {
+  console.log("ae");
 }
