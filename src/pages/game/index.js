c1dd91a1e37aa60f7f9e0b599a5669f64a2c12c2
Raphael Divetta Bronzo <divetta@gmail.com>
2019-01-29 11:59:11 +0000

    Fim da aula 026

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,24 +1,35 @@
 const $root = document.querySelector("#root");
 
 const $cardsWrapper = createCardsWrapper();
-const $memoryCard = createMemoryCard(
-  "img/icon-collabcode.png",
-  "Gueio mascote da CollabCode",
-  ""
-);
-const $memoryCardFront = createMemoryCard(
-  "img/icon-c.png",
-  "Livro de C++",
-  "-front"
-);
 
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCard);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardFront);
+const $memoryCardC = createMemoryCard({
+  nameClass: "-front",
+  src: "img/icon-c.png",
+  alt: "Livro de C++"
+});
+const $memoryCardJS = createMemoryCard({
+  src: "img/icon-js.png",
+  alt: "Livro de JavaScript",
+  nameClass: "-front"
+});
+const $memoryCardJava = createMemoryCard({
+  src: "img/icon-java.png",
+  alt: "Livro de Java",
+  nameClass: "-front"
+});
+const $memoryCardWoman = createMemoryCard({
+  src: "img/icon-woman.png",
+  alt: "Icone de uma mulher codando",
+  nameClass: "-front"
+});
+
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardWoman);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
+$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardWoman);
 
 $root.insertAdjacentElement("beforeend", $cardsWrapper);
