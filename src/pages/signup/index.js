271e5ca13e2d8f53dd4c3fc727855fffb4519f3f
Raphael Divetta Bronzo <divetta@gmail.com>
2019-03-17 20:14:41 -0300

    Fim da aula 039

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,8 +1,8 @@
 (function() {
   const $root = document.querySelector("#root");
 
-  const $loginButton = flatButton.render("Log in", "flat-button");
-  const $signinButton = flatButton.render("Sign in", "flat-button -active");
+  const $loginButton = flatButton.render("Log in");
+  const $signinButton = flatButton.render("Sign up", true);
 
   $root.insertAdjacentHTML("beforeend", $loginButton);
   $root.insertAdjacentHTML("beforeend", $signinButton);
